e90ffa81764f968663f6875b768e5e358f103cc9
georgeibeawuchi <79763444+georgeibeawuchi@users.noreply.github.com>
2023-07-24 16:05:03 +0100

    Add files via upload

--- While Loop Structure.py
+++ While Loop Structure.py
@@ -1,12 +1,25 @@
-n = int(input("Enter a number: "))
 
-numberlist = []
+# n is declared as 0 firstly
+n = 0
 
+# user is asked politely to enter a number
+n = int(input("Please enter a number \n"))
+
+# total number a sum of mathematics is declared.
+totalnum = n
+sum = 0
+
+# while loop with -1 as a stoppage to its function
 while n != -1:
-    numberlist.append(n)
-    
-    n = int(input())
+ 
+      n = int(input("Please enter a number \n"))
+      sum += n
+      #n -= 1
 
-    numbersum = sum(numberlist)
+      print(sum)
 
-print(f"The averge of these numbers are: {numbersum / len(numberlist)}")
+# average equation
+average = sum / totalnum
+
+# this prints the average.
+print(average)
